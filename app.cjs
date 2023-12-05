179c6f1115cdad83d8a8d8721d32fe486a760924
josueJURE <josue.jure@gmail.com>
2023-12-05 08:27:19 +0000

    write a working chatbot

--- app.cjs
+++ app.cjs
@@ -1,38 +1,115 @@
+const dotenv  = require("dotenv");
+const {Client} = require("discord.js");
+const {OpenAI} = require("openai");
 
-const OpenAI = require("openai")
-const dotenv  = require("dotenv")
-const {Client, GatewayIntentBits} = require("discord.js");
 dotenv.config()
-const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]})
-const token = process.env.DISCORD_TOKEN
 
 
 
 
-client.on("ready", (x) => {
-  console.log(`${x.user.tag} is ready`);
-  client.user.setActivity("hello how you doing")
+const client = new Client({
+  intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"]
 
 })
 
-client.login(token)
+client.on("ready", () => {
+  console.log("the bot is online");
+});
+
+const IGNORE_PREFIX = "!";
+const CHANNELS = ['1118590826717659297'];
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_API_KEY,
+
+})
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if(message.content.startsWith(`${IGNORE_PREFIX}`)) return;
+  // if(!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
+
+  await message.channel.sendTyping();
+
+  const sendTypingInterval = setInterval(() => {
+    message.channel.sendTyping();
+  }, 5000);
+
+  let conversation = [];
+conversation.push({
+  role: 'system',
+  content: "Chat GPT is a friendly chatbot"
+})
+
+  console.log(message.channel.messages);
+
+  let prevMessages = await message.channel.messages.fetch({limit: 10});
+  prevMessages.reverse();
+
+prevMessages.forEach(msg => {
+  if(msg.author.bot && msg.author.id !== client.user.id) return;
+  if(msg.content.startsWith(IGNORE_PREFIX)) return;
+
+  const username = msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
+
+  if(msg.author.id === client.user.id) {
+    conversation.push({
+      role: "assistant",
+      name: username,
+      content: msg.content,
+
+    })
+    return;
+  }
+
+  conversation.push({
+    role: "user",
+    name: username,
+    content: msg.content,
+  })
+ 
+}) 
+ 
 
 
 
 
-const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
-
-  console.log(completion.choices[0]);
-
-}
+  const response = await openai.chat.completions.create({
+    model: "gpt-4",
+    messages: conversation,
 
 
+  })
+  .catch((error) => {console.error(error)})
 
-main();
+  clearInterval(sendTypingInterval);
+
+  if(!response) {
+    message.reply("I'm having trouble with the OPEN API");
+    return;
+  }
+
+  const responseMessage = response.choices[0].message.content;
+  const chunkSizeLimit = 2000;
+
+  for(let i = 0; i < responseMessage.length; i += chunkSizeLimit) {
+    const chunk = responseMessage.substring(i, i + chunkSizeLimit);
+    await message.reply(chunk);
+  }
+
+
+
+  message.reply(response.choices[0].message.content)
+})
+
+
+
+
+
+
+client.login((process.env.DISCORD_TOKEN))
+
+
+
 
---
+++ index.js
@@ -0,0 +1,11 @@
+// Load environment variables from a .env file
+// require("dotenv").config();
+
+// const main = require("./app");
+// // Import necessary modules from the discord.js library
+// const { Client, GatewayIntentBits } = require("discord.js");
+
+
+
+
+
